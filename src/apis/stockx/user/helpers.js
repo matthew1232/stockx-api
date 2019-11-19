a3fa634daeba811dcce41d7bbaf771778fc180a3
matthew1232 <matthew.mulfingers@gmail.com>
2019-11-18 17:15:22 -0800

    Fixed callback submission

--- src/apis/stockx/user/helpers.js
+++ src/apis/stockx/user/helpers.js
@@ -95,24 +95,27 @@ export const submitCredentials = async ({ request, headers, jar, proxy, state, c
 
 export const checkStatus = async ({ request, headers, jar, proxy, wa, wresult, wctx }) => {
   try {
-    const res = await request(`https://accounts.stockx.com/login/callback`, {
-      body: encodeURI(`wa=${wa}&wresult=${wresult}&wctx=${wctx}`),  
-      followAllRedirects: true,
-      followRedirect: true,  
-      headers: {
-        ...headers,
-        accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-        authority: 'accounts.stockx.com',
-        origin: 'https://accounts.stockx.com',
-        'content-type': 'application/x-www-form-urlencoded',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-      },
-      jar,
-      method: 'POST',
-      proxy,
-      resolveWithFullResponse: true,
-      simple: false,
+     const res = await request(`https://accounts.stockx.com/login/callback`, {
+        body: encodeURI(`wa=${wa}&wresult=${wresult}&wctx=${wctx}`),  
+        followAllRedirects: true,
+        followRedirect: true,  
+        headers: {
+          'authority': 'accounts.stockx.com',
+          'cache-control': 'max-age=0',
+          'origin': 'https://accounts.stockx.com',
+          'upgrade-insecure-requests': '1',
+          'content-type': 'application/x-www-form-urlencoded',
+          'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-user': '?1',
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+          'sec-fetch-site': 'same-origin'
+        },
+        jar,
+        method: 'POST',
+        proxy,
+        resolveWithFullResponse: true,
+        simple: false,
     });
 
     const { statusCode } = res;

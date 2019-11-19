f6cc92abb2bddd4cfb6e583ff6cb942b5cc8f598
Matthew Wall <matthew.wallt@gmail.com>
2019-11-19 18:19:08 -0500

    updated examples to update ask

--- examples/index.js
+++ examples/index.js
@@ -4,11 +4,10 @@ const StockX = require('../dist').default;
 
 const { username, password } = process.env;
 
-const stockXController = new StockX();
+const stockXController = new StockX({ currency: 'AUD' });
 // if you're using charlesproxy, make sure it's launched and use the controller below!
 // const stockXController = new StockX({ proxy: '127.0.0.1:8888' });
 
-// login to account
 stockXController.user
   .login({ username, password })
   .then(() => stockXController.products.search('yeezy', { limit: 1 }))
@@ -27,6 +26,12 @@ stockXController.user
   .then(ask => 
   {
     console.log(`Successfully placed ask: ${ask.chainId}!`);
+
+    return stockXController.asks.update(ask, { amount: 50000000 });
+  })
+  .then(updatedAsk => 
+  {
+    console.log(`Successfully updated ask: ${updatedAsk.chainId}`)
   })
   .catch(console.error);
 

d7e8fd507289d1dd0cf29924945a98a55dac3a92
matthew1232 <matthew.mulfingers@gmail.com>
2019-11-20 13:23:13 -0800

    Added deleting bids, implemented checking status from utils
    I added the option to delete bids, and implemented the checkStatus from the util errors file. I also removed the error bubbling and added the `body` param to the errors so the errors stay consistent across the board.

--- src/apis/stockx/bids/index.js
+++ src/apis/stockx/bids/index.js
@@ -1,7 +1,8 @@
 import moment from 'moment';
 
 import Base from '../../base';
-import { randomInclusive, errors } from '../../../utils';
+import { randomInclusive } from '../../../utils';
+import { checkStatus } from '../../../utils';
 
 export default class Bids extends Base {
   // TODO!
@@ -11,55 +12,48 @@ export default class Bids extends Base {
     const { amount } = options;
     const { bearer, currency, headers, jar, proxy, request } = this.context;
     const expiresAt = moment().add(30, 'days').utc().format();
+    const { chainId, skuUuid } = bid;
 
-    try {
-      let { chainId, skuUuid } = bid;
-      if (!amount || !skuUuid || !chainId) {
-        const error = new Error('Invalid amount product id, and/or ask id!');
-        error.status = 404;
-        throw error;
-      }
-
-      if (!bearer) {
-        const error = new Error('Please login first!');
-        error.status = 401;
-        throw error;
-      }
-
-      const res = await request('https://stockx.com/api/portfolio?a=bid', {
-        headers: {
-          ...headers,
-          authorization: `Bearer ${bearer}`,
-          'content-type': 'application/json',
-        },
-        jar,
-        json: {
-          PortfolioItem: {
-            localAmount: amount,
-            skuUuid,
-            localCurrency: currency,
-            expiresAt,
-            chainId,
-          },
-        },
-        method: 'POST',
-        proxy,
-        resolveWithFullResponse: true,
-        simple: false,
-      });
-
-      const { statusCode, body } = res;
-      if (!statusCode || (statusCode && statusCode !== 200)) {
-        const err = new Error('Invalid status code!');
-        err.status = statusCode || 404;
-        throw err;
-      }
-
-      ({ PortfolioItem: { chainId, skuUuid }} = body);
-      return { chainId, skuUuid };
-    } catch (error) {
-      return errors(error, 'update bid');
+    if (!amount || !skuUuid || !chainId) {
+      const error = new Error('Invalid amount product id, and/or ask id!');
+      error.status = 404;
+      throw error;
     }
+
+    if (!bearer) {
+      const error = new Error('Please login first!');
+      error.status = 401;
+      throw error;
+    }
+
+    const res = await request('https://stockx.com/api/portfolio?a=bid', {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${bearer}`,
+        'content-type': 'application/json',
+      },
+      jar,
+      json: {
+        PortfolioItem: {
+          localAmount: amount,
+          skuUuid,
+          localCurrency: currency,
+          expiresAt,
+          chainId,
+        },
+      },
+      method: 'POST',
+      proxy,
+      resolveWithFullResponse: true,
+      simple: false,
+    });
+
+    checkStatus(res);
+
+    const { body } = res;
+    const { PortfolioItem: { chainId, skuUuid } } = body;
+    
+    return { chainId, skuUuid };
   }
 
   async place(product, options = {}) {
@@ -67,64 +61,92 @@ export default class Bids extends Base {
     const { bearer, currency, headers, jar, proxy, request } = this.context;
     const expiresAt = moment().add(30, 'days').utc().format();
 
-    try {
-      if (!amount || !size || !product) {
-        const error = new Error('Invalid product, amount, and/or size!');
-        error.status = 404;
-        throw error;
-      }
-
-      if (!bearer) {
-        const error = new Error('Please login first!');
-        error.status = 401;
-        throw error;
-      }
-
-      const desiredSize = /random/i.test(size) ? randomInclusive(product.variants) : product.variants.find(v => v.size === size);
-
-      if (!desiredSize || (desiredSize && !desiredSize.uuid)) {
-        const error = new Error('Size not found!');
-        error.status = 404;
-        throw error;
-      }
-
-      const { uuid } = desiredSize;
-
-      const res = await request('https://stockx.com/api/portfolio?a=bid', {
-        headers: {
-          ...headers,
-          authorization: `Bearer ${bearer}`,
-          'content-type': 'application/json',
-        },
-        jar,
-        json: {
-          PortfolioItem: {
-            localAmount: amount,
-            skuUuid: uuid,
-            localCurrency: currency,
-            expiresAt,
-          },
-        },
-        method: 'POST',
-        proxy,
-        resolveWithFullResponse: true,
-        simple: false,
-      });
-
-      const { statusCode, body } = res;
-      if (!statusCode || (statusCode && statusCode !== 200)) {
-        const err = new Error('Invalid response code!');
-        err.status = statusCode || 404;
-        throw err;
-      }
-
-      const { PortfolioItem: { chainId, skuUuid } } = body;
-      return { chainId, skuUuid };
-    } catch (error) {
-      return errors(error, 'place bid');
+    if (!amount || !size || !product) {
+      const error = new Error('Invalid product, amount, and/or size!');
+      error.status = 404;
+      throw error;
     }
+
+    if (!bearer) {
+      const error = new Error('Please login first!');
+      error.status = 401;
+      throw error;
+    }
+
+    const desiredSize = /random/i.test(size) ? randomInclusive(product.variants) : product.variants.find(v => v.size === size);
+
+    if (!desiredSize || (desiredSize && !desiredSize.uuid)) {
+      const error = new Error('Size not found!');
+      error.status = 404;
+      throw error;
+    }
+
+    const { uuid } = desiredSize;
+
+    const res = await request('https://stockx.com/api/portfolio?a=bid', {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${bearer}`,
+        'content-type': 'application/json'
+      },
+      jar,
+      json: {
+        PortfolioItem: {
+          localAmount: amount,
+          skuUuid: uuid,
+          localCurrency: currency,
+          expiresAt
+        }
+      },
+      method: 'POST',
+      proxy,
+      resolveWithFullResponse: true,
+      simple: false,
+    });
+
+    checkStatus(res);
+
+    const { body } = res;
+    const { PortfolioItem: { chainId, skuUuid } } = body;
+
+    return { chainId, skuUuid };
   }
 
   // TODO!
-  async remove(options = {}) {}
+  async remove(bid = {}) {
+    const bidID = bid.chainId;
+    const { bearer, headers, jar, proxy, request } = this.context;
+
+    if (!bidID){
+      const err = new Error("No bid ID found!");
+      err.status = 404;
+      throw err;
+    };
+
+    const URL = `https://stockx.com/api/portfolio/${bidId}`;
+
+    const res = await request(URL, {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${bearer}`,
+        'content-type': 'application/json'
+      },
+      method: 'DELETE',
+      jar, 
+      proxy,
+      json: {
+        "chain_id": bidID.toString(),
+        "notes": "Customer Removed Bid"
+      },
+      simple: false,
+      resolveWithFullResponse: true
+    });
+
+    checkStatus(res);
+  
+    const { body } = res;
+    const { PortfolioItem: { chainId, skuUuid } } = body;
+
+    return { chainId, skuUuid };
+  }
 }

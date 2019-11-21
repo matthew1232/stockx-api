277c80c47ca8b8c0d8ba707f9d94cf7ca43c0bb7
matthew1232 <matthew.mulfingers@gmail.com>
2019-11-21 12:34:24 -0800

    Include 304 as accepted status code

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,25 +1,7 @@
-/*export default (thrown, name) => {
-  if (thrown.name === 'StatusCodeError') {
-    if (thrown.error) {
-        const { statusCode, message } = thrown.error;
-        const error = new Error(`Unable to ${name}: ${message}`);
-        error.status = statusCode || thrown.status || 404;
-        throw error;
-    }
-    const { statusCode, message } = thrown;
-    const error = new Error(`Unable to ${name}: ${message}`);
-    error.status = statusCode || thrown.status || 404;
-    throw error;
-  }
-  const error = new Error(`Unable to ${name}: ${thrown.message}`);
-  error.status = thrown.status || 404;
-  throw thrown;
-}*/
-
 export const checkStatus = (res) => {
   const { status, body } = res;
   
-  if (status == 200) return;
+  if (status == 200 || status == 304) return;
 
   const err = new Error("Status code error");
   err.status = status || 404;

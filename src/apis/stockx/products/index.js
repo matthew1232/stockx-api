40846b445803ebaf8b6fd0c4308900c4a1a7df9a
matthew1232 <matthew.mulfingers@gmail.com>
2019-11-24 20:57:23 -0800

    Add error handling from utils
    Allowed objects from products.search, fixed error handling and removed error bubbling that was unneeded.

--- src/apis/stockx/products/index.js
+++ src/apis/stockx/products/index.js
@@ -1,89 +1,75 @@
 
 import Base from '../../base';
-import { filterAndLimit, errors } from '../../../utils';
+import { filterAndLimit, checkStatus } from '../../../utils';
 
 export default class Products extends Base {
   async search(query, options = {}) {
     const { limit, type } = options;
     const { headers, proxy, request } = this.context;
+    const url = `https://stockx.com/api/browse?&_search=${query}&dataType=product`;
 
-    let url = `https://stockx.com/api/browse?&_search=${query}`;
+    const res = await request(url, {
+      headers,
+      json: true,
+      proxy,
+      resolveWithFullResponse: true,
+      simple: false,
+    });
+    
+    checkStatus(res);
 
-    if (type) {
-      url += `&dataType=${type}`;
-    }
+    const { Products } = res.body;
+    const products = filterAndLimit(Products, null, limit);
 
-    try {
-      const res = await request(url, {
-        headers,
-        json: true,
-        proxy,
-        resolveWithFullResponse: true,
-        simple: false,
-      });
+    return products.map(product => {
+      const image = new URL(product.media.imageUrl, 'https://stockx.com').href;
 
-      const { Products } = res.body;
-      const products = filterAndLimit(Products, null, limit);
+      const { title, retailPrice, releaseDate, styleId, urlKey, market } = product;
+      const { productUuid } = market;
 
-      return products.map(product => {
-        const image = new URL(product.media.imageUrl, 'https://stockx.com').href;
-
-        const { title, retailPrice, releaseDate, styleId, urlKey, market } = product;
-        const { productUuid } = market;
-
-        return { title, retailPrice, releaseDate, styleId, productUuid, image, urlKey, market };
-      });
-    } catch (error) {
-      return errors(error, 'search products');
-    }
+      return { title, retailPrice, releaseDate, styleId, productUuid, image, urlKey, market };
+    });
   }
 
   async details(product) {
-    try {
-      const { currency, headers, request } = this.context;
-      const { pathname } = new URL(product, 'https://stockx.com');
-      
-      if (!pathname) {
-        const err = new Error('Invalid product path!');
-        err.status = 404;
-        throw err;
-      }
-      
-      let url = `https://stockx.com/api/products${pathname}?includes=market&currency=${currency}`;
+    const { currency, headers, request } = this.context;
+    const productURL = typeof product == 'string' ? product : product.urlKey;
+    const { pathname } = new URL(productURL, 'https://stockx.com');
 
-      const res = await request(url, {
-        headers,
-        json: true,
-        resolveWithFullResponse: true,
-        simple: false,
-      });
-
-      const { statusCode, body } = res;
-      if (!statusCode || (statusCode && statusCode !== 200)) {
-        const err = new Error('Invalid response code!');
-        err.status = statusCode || 404;
-        throw err;
-      }
-
-      const { Product } = body;
-      const { children, title, urlKey, styleId, uuid } = Product;
-
-      const variants = [];
-      Object.values(children).map(p => {
-        const { shoeSize: size, uuid, market } = p;
-        variants.push({ size, uuid, market });
-      });
-
-      return {
-        title,
-        urlKey,
-        styleId,
-        uuid,
-        variants,
-      };
-
-    } catch (error) {
-      return errors(error, 'fetch product details');
+    if (!pathname) {
+      const err = new Error('Invalid product path!');
+      err.status = 404;
+      err.body = '';
+      throw err;
     }
+
+    const url = `https://stockx.com/api/products${pathname}?includes=market&currency=${currency}`;
+
+    const res = await request(url, {
+      headers,
+      json: true,
+      resolveWithFullResponse: true,
+      simple: false,
+    });
+
+    checkStatus(res);
+    
+    const { body } = res;
+    const { Product } = body;
+    const { children, title, urlKey, styleId, uuid } = Product;
+
+    const variants = [];
+    Object.values(children).map(p => {
+      const { shoeSize: size, uuid, market } = p;
+      variants.push({ size, uuid, market });
+    });
+
+    return {
+      title,
+      urlKey,
+      styleId,
+      uuid,
+      variants,
+    };
   }
 };
